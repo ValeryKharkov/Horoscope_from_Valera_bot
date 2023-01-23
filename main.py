16abd928f04aed693b3cd60a41639768ea26f56c
ValeryKharkov <kharkov.valery@gmail.com>
2023-01-23 23:00:28 +0300

    Изменена концепция с "Гороскоп" на "Цитатник"

--- main.py
+++ main.py
@@ -1,65 +1,55 @@
-# Подключаем модуль случайных чисел
-import random
-# Подключаем модуль для Телеграма
+# Подключение модуля для Телеграма
 import telebot
-# Указываем токен
+# Указание токена
 bot = telebot.TeleBot('5306367079:AAFoHb4twE5rlH5HoSl6pEaIZN9oDRQSSE0')
-# Импортируем типы из модуля, чтобы создавать кнопки
+# Импорт типов из модуля, чтобы создавать кнопки
 from telebot import types
-# Заготовки для трёх предложений
-list_ = ["Ты умрёшь когда нибудь, извини.", "Умирая, ты не сможешь с собой забрать булочки.", "Я не знаю!", "Ты что, веришь в гороскоп????"]
-# second = ["Умирая, ты не сможешь с собой забрать булочки."]
-# second_add = ["Я не знаю!"]
-# third = ["Ты что, веришь в гороскоп????"]
+# Импорт цитат из модуля
+import quote
+# Подключение модуля случайных чисел
+import random
+
+
 # Метод, который получает сообщения и обрабатывает их
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-    # Если написали «Привет»
-    if message.text == "Привет":
-        # Пишем приветствие
-        bot.send_message(message.from_user.id, "Привет, сейчас я расскажу твоё будущее по знаку зодиака.")
-        # Готовим кнопки
+    # Если написали «ты пидор»
+    if message.text == "ты пидор" or message.text == "Ты пидор":
+        # Установка приветствия
+        bot.send_message(message.from_user.id,
+                         "Привет, сейчас ты почитаешь цитаты от людей, решивших найти средство против "
+                         "несправедливости и лицемерия общества.")
+        # Подготовка кнопок
         keyboard = types.InlineKeyboardMarkup()
-        # По очереди готовим текст и обработчик для каждого знака зодиака
-        key_oven = types.InlineKeyboardButton(text='Овен', callback_data='zodiac')
-        # И добавляем кнопку на экран
-        keyboard.add(key_oven)
-        key_telec = types.InlineKeyboardButton(text='Телец', callback_data='zodiac')
-        keyboard.add(key_telec)
-        key_bliznecy = types.InlineKeyboardButton(text='Близнецы', callback_data='zodiac')
-        keyboard.add(key_bliznecy)
-        key_rak = types.InlineKeyboardButton(text='Рак', callback_data='zodiac')
-        keyboard.add(key_rak)
-        key_lev = types.InlineKeyboardButton(text='Лев', callback_data='zodiac')
-        keyboard.add(key_lev)
-        key_deva = types.InlineKeyboardButton(text='Дева', callback_data='zodiac')
-        keyboard.add(key_deva)
-        key_vesy = types.InlineKeyboardButton(text='Весы', callback_data='zodiac')
-        keyboard.add(key_vesy)
-        key_scorpion = types.InlineKeyboardButton(text='Скорпион', callback_data='zodiac')
-        keyboard.add(key_scorpion)
-        key_strelec = types.InlineKeyboardButton(text='Стрелец', callback_data='zodiac')
-        keyboard.add(key_strelec)
-        key_kozerog = types.InlineKeyboardButton(text='Козерог', callback_data='zodiac')
-        keyboard.add(key_kozerog)
-        key_vodoley = types.InlineKeyboardButton(text='Водолей', callback_data='zodiac')
-        keyboard.add(key_vodoley)
-        key_ryby = types.InlineKeyboardButton(text='Рыбы', callback_data='zodiac')
-        keyboard.add(key_ryby)
-        # Показываем все кнопки сразу и пишем сообщение о выборе
-        bot.send_message(message.from_user.id, text='Выбери свой знак зодиака', reply_markup=keyboard)
+        # По очереди готовим текст и обработчик для каждого автора
+        key_carlin = types.InlineKeyboardButton(text='Карлин', callback_data='Сarlin')
+        # Добавление кнопки на экран
+        keyboard.add(key_carlin)
+        key_ranevskaya = types.InlineKeyboardButton(text='Раневская', callback_data='Ranevskaya')
+        keyboard.add(key_ranevskaya)
+        # Установка всех кнопок сразу и написание сообщения о выборе
+        bot.send_message(message.from_user.id, text='Выбери автора', reply_markup=keyboard)
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши Привет")
+        bot.send_message(message.from_user.id, "Напиши \"ты пидор\"")
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
 # Обработчик нажатий на кнопки
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    # Если нажали на одну из 12 кнопок — выводим гороскоп
-    if call.data == "zodiac":
-        # Формируем гороскоп
-        msg = random.choice(list_)
-        # Отправляем текст в Телеграм
+    # Если нажата одна из кнопок — выводим цитату
+    if call.data == "Сarlin":
+        # Формирование цитат
+        msg = random.choice(quote.carlin_list)
+        # Отправка текста в Телеграм
         bot.send_message(call.message.chat.id, msg)
-# Запускаем постоянный опрос бота в Телеграме
+    elif call.data == "Ranevskaya":
+        # Формирование цитат
+        msg = random.choice(quote.ranevskaya_list)
+        # Отправка текста в Телеграм
+        bot.send_message(call.message.chat.id, msg)
+
+
+# Запуск постоянного опроса бота в Телеграме
 bot.polling(none_stop=True, interval=0)
